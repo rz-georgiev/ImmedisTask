d0bc5c7bfbb91b6b9ba3f0d7f32100791172041c
RGeorgiev <rzgeorgiev@outlook.com>
2020-07-25 13:30:59 +0300

    Written comments service logic

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,24 @@
+﻿using ImmedisTask.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImmedisTask.Controllers
+{
+    public class CommentController : Controller
+    {
+        private ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public IActionResult Index(int employeeId)
+        {
+            var comments = _commentService.GetByEmployeeId(employeeId);
+
+
+
+            return View();
+        }
+    }
+}
